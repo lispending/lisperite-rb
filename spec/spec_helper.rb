273c43a59084314043e06a7e2968365adc341583
Ricardo Valeriano <mister.sourcerer@gmail.com>
2025-09-14 20:44:49 -0300

    Prepare for better testing and cli experiments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,23 @@
 
 require "lisperite"
 
+module Lisperite
+  module SpecHelper
+    FIXTURES_PATH = File.expand_path("../fixtures/", __FILE__).freeze
+
+
+    def fixture_path(file_name)
+      File.join(FIXTURES_PATH, file_name)
+    end
+
+    def fixture(file_name)
+      path = fixture_path(file_name)
+      raise "no file: #{fixture_path(path)}" if !File.exist?(fixture_path(path))
+      File.read(path)
+    end
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -12,4 +29,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include Lisperite::SpecHelper
 end

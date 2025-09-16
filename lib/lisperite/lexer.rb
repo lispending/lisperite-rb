3a82a1aaf6bff1fd3e51df4871d0d57b870bb745
Ricardo Valeriano <mister.sourcerer@gmail.com>
2025-09-16 00:09:42 -0300

    a little bit of a rollback...

--- lib/lisperite/lexer.rb
+++ lib/lisperite/lexer.rb
@@ -4,45 +4,13 @@ module Lisperite
 
     def initialize(source)
       @reader = Lisperite::Reader.new(source)
-      @line = 1
-      @column = 1
     end
 
     def run
       tokens = []
       until reader.eof?
-        consume while reader.peek == "\s"
-        token = next_token
-        tokens << token if token
       end
-
       tokens
-    end
-
-    private
-
-    def next_token
-      return if reader.eof?
-      case reader.peek
-      when "..."
-        nil
-      else
-        atom
-      end
-    end
-
-    def atom
-      str = ""
-      start = {line: @line, column: @column}
-      while !reader.eof? && consume
-        break if reader.current == "\s"
-        str << reader.current
-      end
-      {type: :atom, value: str, position: {start: start, end: {line: @line, column: @column}}}
-    end
-
-    def consume
-      reader.next.tap { @column += 1 }
     end
   end
 end

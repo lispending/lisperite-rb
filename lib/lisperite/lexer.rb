6d8db26d4fd5a462037a22677a8cab04dfbf9033
Ricardo Valeriano <mister.sourcerer@gmail.com>
2025-09-15 22:07:33 -0300

    [wip] recognizes single atom.

--- lib/lisperite/lexer.rb
+++ lib/lisperite/lexer.rb
@@ -4,6 +4,38 @@ module Lisperite
 
     def initialize(source)
       @reader = Lisperite::Reader.new(source)
+      @line = 1
+      @column = 0
+    end
+
+    def run
+      tokens = []
+      until reader.eof?
+        tokens << next_token
+      end
+
+      tokens
+    end
+
+    private
+
+    def next_token
+      return if reader.eof?
+
+      case reader.peek
+      when "..."
+        nil
+      else
+        atom
+      end
+    end
+
+    def atom
+      str = ""
+      while !reader.eof? && reader.next
+        str << reader.current
+      end
+      {type: :atom, value: str}
     end
   end
 end
--- spec/lisperite/lexer_atom_spec.rb
+++ spec/lisperite/lexer_atom_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe Lisperite::Lexer do
-  it "recognizes a single atom"
+  it "recognizes a single atom" do
+    tokens = described_class.new("lisperite").run
+
+    expect(tokens.first[:type]).to eq :atom
+    expect(tokens.first[:value]).to eq "lisperite"
+  end
+
   it "recognizes multiple atoms separated by space"
   it "ignores unsignificant spaces"
   it "keeps track of columns where atoms start and end"
